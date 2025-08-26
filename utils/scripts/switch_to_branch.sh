a7b0f31e5a12d0ec1047b3f1487ed7a4c4bcdccd
QwertyJacob <jesusfcevallos@gmail.com>
2025-08-26 11:44:21 +0200

    refacs

--- utils/scripts/switch_to_branch.sh
+++ utils/scripts/switch_to_branch.sh
@@ -26,6 +26,7 @@ for image in "${TARGET_IMAGES[@]}"; do
     for container in $containers; do
         container_name=$(docker inspect --format='{{.Name}}' "$container" | sed 's/\///')
         echo "Switching to branch $BRANCH in container $container_name ($container)..."
+        docker exec -it "$container" /bin/bash -c "git fetch"
         docker exec -it "$container" /bin/bash -c "git checkout $BRANCH"
     done
 
